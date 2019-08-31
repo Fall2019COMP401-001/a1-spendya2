7e678eb30e25e51ccee4078ffb8fea96bb41d7a9
suraj <suraj@DESKTOP-MITG9B7>
2019-08-31 00:19:55 -0400

    Finished Novice 1

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -7,8 +7,67 @@ public class A1Adept {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		process(scan);
+	}
+	
+	public static void process(Scanner s) {
+		int numOfStoreItems = s.nextInt();
 		
+		StoreItems[] storeItems = new StoreItems[numOfStoreItems];
+		
+		for (int i = 0; i < numOfStoreItems; i++) {
+			storeItems[i].name = s.next();
+			storeItems[i].price = s.nextDouble();
+		}
+		
+		int numOfCusts = s.nextInt();
+		Customers[] customers = new Customers[numOfCusts];
+		
+		
+		
+		double total = 0;
+		
+		for (int i = 0; i < numOfCusts; i++) {
+			customers[i].firstName = s.next();
+			customers[i].lastName = s.next();
+			customers[i].totalItems = s.nextInt();
+		
+			for  (int j = 0; j < customers[i].totalItems; j++) {
+				int quantity = s.nextInt();
+				String itemName = s.next();
+				for (int k = 0; k < numOfStoreItems; k++) {
+					if (itemName.contentEquals(storeItems[k].name)) {
+						double cost = quantity * storeItems[k].price;
+						total += cost;
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < customers[i].totalItems; i++) {
+			customers[i].totalCost = total;
+			double maxCost = customers[i].totalCost;
+			String maxName = customers[i].firstName + customers[i].lastName;
+			for (int j = 0; j < numOfCusts; j++) {
+				if (customers[j].totalCost > customers[j+1].totalCost) {
+					maxCost = customers[j].totalCost;
+					maxName = customers[j].firstName + " " + customers[j].lastName;
+				}
+			}
+			System.out.println("Biggest: " + maxName + " (" + maxCost + ")" );
+		}
+		
+		for (int i = 0; i < customers[i].totalItems; i++) {
+			double minCost = customers[i].totalCost;
+			String minName = customers[i].firstName + customers[i].lastName;
+			for (int j = 0; j < numOfCusts; j++) {
+				if (customers[j].totalCost < customers[j+1].totalCost) {
+					minCost = customers[j].totalCost;
+					minName = customers[j].firstName + " " + customers[j].lastName;
+				}
+				System.out.println("Smallest: " + minName + " (" + minCost + ")" );
+			}
+		}
 	}
 }
+
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,25 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
-		
+		process(scan);
 	}
+				
+		public static void process(Scanner s) {
+			int customerCount = s.nextInt();
+			
+			for(int i = 0; i <= customerCount; i++) {
+				double total = 0;
+				String firstName = s.next();
+				String lastName = s.next();
+				int groceries = s.nextInt();
+				for (int j = 0; j < groceries; j++) {
+					int quantity = s.nextInt();
+					String itemName = s.next();
+					double price = s.nextDouble();
+					double cost = price * quantity;
+					total += cost;
+				}
+				System.out.println(" " + firstName.charAt(0) + "." + " " + lastName + ": " + total);
+			}
+		}
 }
